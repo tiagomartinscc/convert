298ff23357b079a5f4116c78c5bdbde90d8761be
Tiago Martins <tiagomartinscc@hotmail.com>
2024-06-25 17:50:42 -0300

    function convert currency

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+//cotação de moedas do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+// obtendo elementos do form
 const form = document.querySelector('form')
 const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
@@ -11,4 +17,22 @@ amount.addEventListener('input', (event) => {
 // capturando o evento de submit do form
 form.onsubmit = (event) => {
   event.preventDefault()
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, 'US$')
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break;    
+
+  }
+}
+
+// função de conversão de moeda
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
 }
