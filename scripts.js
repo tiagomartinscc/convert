20c3a0675e10b624e59657785639f0905df0aa9b
Tiago Martins <tiagomartinscc@hotmail.com>
2024-06-25 17:39:33 -0300

    getting form values

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const currency = document.getElementById('currency')
 
 // pegar apenas numeros
 amount.addEventListener('input', (event) => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// capturando o evento de submit do form
+form.onsubmit = (event) => {
+  event.preventDefault()
+}
